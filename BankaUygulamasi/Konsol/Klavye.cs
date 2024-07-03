b63ad2162785d8cffeea28e75432f12ba1b524b1
Mustafa KURAL <mustafakural@outlook.com>
2024-07-03 10:27:05 +0300

    first commit

---
+++ BankaUygulamasi/Konsol/Klavye.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Compression;
+
+namespace BankaUygulamasi.Konsol;
+
+//Yardımcı metotlar içeren sınıflar genellikle static oluşturulur
+//Tüm projeden bu static sınıflar erişilir ve aynı nesneyi kullanır
+//**static sınıfların tüm üyeleri de static olur
+public static class Klavye
+{
+    public static string StrOku(string mesaj)
+    {
+        Console.Write(mesaj);
+        return Console.ReadLine();
+    }
+
+    public static int IntOku(string mesaj)
+    {
+        Console.Write(mesaj);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    public static double DoubleOku(string mesaj)
+    {
+        Console.Write(mesaj);
+        return Convert.ToDouble(Console.ReadLine());
+    }
+}
---
+++ BankaUygulamasi/Program.cs
@@ -0,0 +1,8 @@
+﻿//ANAPROGRAM
+
+using BankaUygulamasi.Konsol;
+
+string ad = Klavye.StrOku("Adınızı girin:");
+int yas = Klavye.IntOku("Yaşınız nedir?");
+
+Console.WriteLine("Merhaba," + ad);
